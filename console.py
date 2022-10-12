3f18bc4465eb560aac580f113a42ecaf6e503d4d
NachoCape <igcapezzolo@hotmail.com>
2022-10-12 16:34:46 -0300

    add task6 (console)

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import cmd, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, *arg):
+        'Quit command to exit the program'
+        return True
+
+    def do_EOF(self, *arg):
+        'EOF command to exit the program'
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
