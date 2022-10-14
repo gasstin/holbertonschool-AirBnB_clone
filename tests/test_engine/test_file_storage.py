12bc7be1d3f0c3bf99dbdf61d74e48d2d65c006d
gasstin <gastonl1994@gmail.com>
2022-10-14 15:32:16 -0300

    fixing test_file_storage

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import unittest
-from models.base_model import BaseModel
+from models import base_model
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     
     def test_file_storage_new(self):
-        my_model = BaseModel()
+        my_model = base_model.BaseModel()
         storage = FileStorage()
         storage.new(my_model)
         self.assertEqual(storage.all()[f"{my_model.__class__.__name__}.{my_model.id}"], my_model)
@@ -19,20 +19,15 @@ class TestFileStorage(unittest.TestCase):
 
     def test_file_storage_save(self):
         _file_path = "file.json"
-        my_model = BaseModel()
+        my_model = base_model.BaseModel()
         storage = FileStorage()
         storage.new(my_model)
         my_model.save()
         self.assertEqual(my_model.save(), 0)
     
     def test_file_storage_reload(self):
-        my_model = BaseModel()
+        my_model = base_model.BaseModel()
         storage = FileStorage()
         storage.new(my_model)
         storage.save()
         self.assertEqual(storage.reload(), 0)
-        
-        
-    # def test_save_storage(self):
-    #     storage = FileStorage()
-    #     storage.save(self)
