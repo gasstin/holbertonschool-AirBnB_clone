239c4bf6494cd689c40d3eeb53b63dd5228bb64a
NachoCape <igcapezzolo@hotmail.com>
2022-10-15 12:16:40 -0300

    update tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,32 +9,36 @@ from models.engine.file_storage import FileStorage
 
 
 class TestBaseModel(unittest.TestCase):
-    
+
     # def test_save(self):
-    #     my_model = BaseModel({'id': '25d98908-41b9-4b8c-9129-2f5ed6ed4c7e', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572), 'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572), '__class__': 'BaseModel'})
+    #     my_model = BaseModel({'id': '25d98908-41b9-4b8c-9129-2f5ed6ed4c7e',
+    #  'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572),
+    #  'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572),
+    #  '__class__': 'BaseModel'})
     #     my_model.save()
-        
+
     def test_created(self):
         my_model = BaseModel()
         my_model.created_at = '2022-10-11'
         self.assertEqual(str(my_model.created_at), '2022-10-11')
-    
+
     def test_update(self):
         my_model = BaseModel()
         my_model.name = "My First Model"
         storage = FileStorage()
         storage.reload()
         self.assertNotEqual(str, my_model.updated_at)
-        
+
     def test_id(self):
         my_model = BaseModel()
         my_model.id = 20
         self.assertEqual(str(my_model.id), "20")
-        
+
     def test_str(self):
-        my_model = BaseModel()
-        self.assertEqual(print(my_model), None)
-    
+        model = BaseModel()
+        prin = f"[{model.__class__.__name__}] ({model.id}) {model.__dict__}"
+        self.assertEqual(prin, model.__str__())
+
     def test_to_dict(self):
         my_model = BaseModel()
         self.assertEqual(type(my_model.to_dict()), dict)
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from imp import reload
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+
+    def test_file_storage_new(self):
+        my_model = BaseModel()
+        storage = FileStorage()
+        storage.new(my_model)
+        prin = f"{my_model.__class__.__name__}.{my_model.id}"
+        self.assertEqual(storage.all()[prin], my_model)
+
+    def test_file_storage_all(self):
+        storage = FileStorage()
+        self.assertEqual(type(storage.all()), dict)
+
+    def test_file_storage_save(self):
+        # _file_path = "file.json"
+        # my_model = BaseModel()
+        # storage = FileStorage()
+        # storage.new(my_model)
+        # my_model.save()
+        # self.assertEqual(my_model.save(), 0)
+        my_model = BaseModel()
+        storage = FileStorage()
+        storage.save()
+        self.assertEqual(my_model.save(), 0)
+
+    def test_file_storage_reload(self):
+        # my_model = BaseModel()
+        # storage = FileStorage()
+        # storage.new(my_model)
+        # storage.save()
+        # self.assertEqual(storage.reload(), 0)
+        storage = FileStorage()
+        storage.reload()
+        self.assertNotEqual(storage.all(), 0)
